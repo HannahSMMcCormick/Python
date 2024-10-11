3fe01536de2218b4e9541e33912aa93027e309d8
HannahSMMcCormick <mccormick.141203@gmail.com>
2024-10-11 23:02:13 +0100

    KthLargestElement

---
+++ KthLargestElement.py
@@ -0,0 +1,24 @@
+
+import heapq
+
+def kth_largest(arr,k):
+    
+    arr = [-elem for elem in arr]
+    
+    heapq.heapify(arr)
+    
+    for i in range(k-1):
+        
+        heapq.heappop(arr)
+        
+    return -heapq.heappop(arr)
+
+
+
+arr1 = [1,4,7,4,6,9,2,1,1,5]
+
+k1 = 4
+
+results = kth_largest(arr1,k1)
+
+print(results)
