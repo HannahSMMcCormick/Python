4f9a5dcd36835fad1702f020ae9f5cb2054e6c2c
HannahSMMcCormick <mccormick.141203@gmail.com>
2024-10-11 23:26:06 +0100

    SymmetricTree

---
+++ SymetricTree.py
@@ -0,0 +1,60 @@
+
+class TreeNode:
+    
+    def __init__(self,val =0, left=None, right=None):
+        
+        self.val = val
+        self.left = left
+        self.right = right
+
+def are_symmetric(root1,root2):
+    
+    if root1 is None and root2 is None:
+        
+        return True
+    
+    elif((root1 is None) != (root2 is None)) or root1.val != root2.val:
+        
+        return False
+
+    else: 
+        
+        return are_symmetric(root1.left,root2.right) and are_symmetric(root1.right, root2.left)
+    
+def is_symmetric(root):
+        
+    if root is None:
+        
+        return True 
+    
+    return are_symmetric(root.left, root.right)
+
+
+def symmetric_tree():
+    
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(3)
+    return root
+
+def non_symmetric_tree():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left.right = TreeNode(3)
+    root.right.right = TreeNode(3)
+    return root
+
+
+symmetric_tree1 = symmetric_tree()
+print(is_symmetric(symmetric_tree1))
+
+non_symmetric_tree1 = non_symmetric_tree()
+print(is_symmetric(non_symmetric_tree1))
+
+empty_tree = None
+print(is_symmetric(empty_tree))
